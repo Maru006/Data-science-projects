b3c99e037d96d84fe454a54797ab25db1293b17d
Maru006 <87828918+Maru006@users.noreply.github.com>
2021-11-14 22:33:33 +0000

    Add files via upload
    basic visualization of forecasted temperature and actual observed readings

---
+++ Visualization.py
@@ -0,0 +1,127 @@
+# visualizing data gathered from Web_Scraping.py
+# ensure plenty of data before engaging with this material
+import seaborn as sns
+
+sns.set_theme()
+from matplotlib import pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth', 1000)
+import sqlite3
+import threading
+from collections import Counter
+
+# connecting to the previous database
+connection = sqlite3.connect('C:\\Users\\Maru\\weather_scraping.db')
+cursor = connection.cursor()
+
+# the following processing turns raw information into a manageable data set for analysis.
+# turning a web-scraped 'date' attribute into a date-time object.
+data_legend = pd.read_sql('SELECT * FROM weather_legend', connection)
+data_legend['date'] = data_legend['date'] + '-2021'
+data_legend.loc[:, 'date'] = pd.to_datetime(data_legend['date'], format='%a-%d-%b-%Y')
+
+data_legend.columns = data_legend.columns.astype('str')
+data_legend['low_temperature'] = data_legend['low_temperature'].astype(float)
+data_legend = data_legend.assign(frequency=0)
+data_legend['high_temperature'] = data_legend['high_temperature'].astype(float)
+
+# grouping date times
+# date times are attributed to n(days)-1 forecast in advance, with 1-1 (0) as actual observation
+counter = Counter(data_legend['date'])
+keys = list(dict.keys(counter))
+groups = []
+frequencies = []
+reindex = []
+for key in keys:
+    groups.append(list(data_legend[data_legend['date'] == key].index))  # groups now returns a list of lists containing index.
+
+for lists in groups:
+    for i in lists:
+        reindex.append(i)  # reindex now returns a list containing individuals indexes from each lists inside groups.
+data_legend = data_legend.reindex(reindex)
+
+for indexes in groups:  # 'indexes' returns a list containing indexes.
+    for i in range(0, len(indexes), 1):  # this counts the length for each list of indexes
+        frequencies.append(i)  # frequencies now return the ranges in ascending maximum len of each indexes
+data_legend = data_legend.assign(frequency=frequencies)
+data_legend['frequency'] = data_legend['frequency'] + 1
+data_legend.sort_index(ascending=True, inplace=True)  # sorting index to represent the order of record acquisition.
+
+
+# run this section for a complete visualization of forecasts including incomplete records of date-series
+# a line plot which shows the average variation between predicted and actual: lowest and highest temperature.
+def legendPlot():
+    print(data_legend)
+    data_legend.columns.astype('str')
+    data_legend['low_temperature'] = data_legend['low_temperature'].astype(float)
+    data_legend['high_temperature'] = data_legend['high_temperature'].astype(float)
+    sns.set_theme()
+    sns.lineplot(data=data_legend, x='date', y='low_temperature', label='Low Temperature')
+    sns.lineplot(data=data_legend, x='date', y='high_temperature', label='High Temperature')
+    plt.title('Weather Legend')
+    plt.show()
+
+
+# partitions dates which have 7 total observations from dates such as today and 6 days in advance that have only 1 variation
+sevens = data_legend[data_legend['frequency'] == 7]
+sevens_date = list(sevens['date'])
+sevens_indexes = []
+for i in sevens_date:
+    sevens_index = data_legend[data_legend['date'] == i].index
+    sevens_indexes.append(list(sevens_index))
+sevens_indexes = [item for sublist in sevens_indexes for item in sublist]  # indexes containing dates with complete records.
+data = data_legend.loc[sevens_indexes]
+data.sort_index(inplace=True)
+sevens_1 = data[data['frequency'] == 1]
+sevens_7 = data[data['frequency'] == 7]
+
+
+# run these section for weather-variability with complete forecast n=7, N=77
+# a boxplot difference between 6-day forecast temperatures vs actual temperature (low and high)
+def box_predicted_7vs1_actual():
+    fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
+    fig.supylabel('Temperature')
+    fig.suptitle('Difference between 6th forecast and actual recorded temperature')
+
+    ax1.boxplot([sevens_1['low_temperature'], sevens_7['low_temperature']])
+    ax1.set_title('Lowest Temperature readings')
+    ax1.set_xticklabels(labels=[])
+
+    ax2.boxplot([sevens_1['high_temperature'], sevens_7['high_temperature']])
+    ax2.set_title('Highest Temperature readings')
+    ax2.set_xticklabels(labels=['Actual', '6th Predicted'])
+
+    plt.subplots_adjust(hspace=.05)
+    plt.tight_layout()
+    plt.show()
+
+
+# a similar comparison over-time.
+def line_predicted_7vs1_actual():
+    fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
+    fig.supylabel('Temperature')
+    fig.supxlabel('Dates')
+    fig.suptitle('Contrast between 6th forecast and actual recorded temperature')
+
+    ax1.plot(sevens_1['date'], sevens_1['low_temperature'],'.-', label='Actual')
+    ax1.plot(sevens_7['date'], sevens_7['low_temperature'],'.-', label='Predicted')
+    ax1.set_title('Lowest Temperature readings')
+    ax1.set_xticklabels(labels=[])
+    ax1.legend()
+
+    ax2.plot(sevens_1['date'], sevens_1['high_temperature'], '.-')
+    ax2.plot(sevens_7['date'], sevens_7['high_temperature'], '.-')
+    ax2.set_title('Highest Temperature readings')
+
+    plt.subplots_adjust(hspace=.05)
+    plt.tight_layout()
+    plt.show()
+
+
+box_predicted_7vs1_actual()
+line_predicted_7vs1_actual()
+connection.close()
---
+++ Web_Scraping.py
@@ -0,0 +1,81 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import pandas as pd
+
+# allows confirmatory viewing of table attributes and values
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+import datetime
+
+date = datetime.datetime.now()
+
+url = requests.get(
+    'https://www.metoffice.gov.uk/weather/forecast/gcpu7tnmn#?nearestTo=Morden%20(Merton)&date=2021-08-16')
+webPageSoup = BeautifulSoup(url.content, 'html.parser')
+weatherTabs = webPageSoup.find_all(class_="forecast-tab")
+tabDay = [weatherTabs.find(class_='tab-day').get_text().strip().replace('Today', date.strftime('%a-%d-%b')).replace(' ', '-') for weatherTabs in weatherTabs]
+lowTemperature = [weatherTabs.find(class_='tab-temp-low').get_text().strip().replace('°', '') for weatherTabs in weatherTabs]
+highTemperature = [weatherTabs.find(class_='tab-temp-high').get_text().strip().replace('°', '') for weatherTabs in weatherTabs]
+summaryText = [weatherTabs.find(class_='summary-text hide-xs-only').get_text().strip() for weatherTabs in weatherTabs]
+
+df = pd.DataFrame(
+    {'Date': tabDay, 'L_Temperature': lowTemperature, 'H_Temperature': highTemperature, 'Notes': summaryText})
+
+# creates a database in the system. Pass your own directory
+connection = sqlite3.connect('C:\\Users\\Maru\\weather_scraping.db')  # change this directory for your database
+cursor = connection.cursor()
+
+
+# creates a table
+def create_legend():
+    legend_sql = 'CREATE TABLE IF NOT EXISTS weather_legend (date, low_temperature , high_temperature , summary_text)'
+    cursor.execute(legend_sql)
+    connection.commit()
+
+
+# fills the weather_legend table wth forecast for each day starting Today to 6 days into the future.
+# this would be carried out each day, which would naturally fill duplicates of previous forecast. Nevertheless the key to recording variations in weather forecast.
+# analysis can be run with enough data: (ideally, where insert_values has been run each day for a month) 10+ dates with the complete 6 day forecast + 1 actual observation.
+def insert_values():
+    for i, row in df.iterrows():
+        insert_to_sql = f'INSERT INTO weather_legend (date, low_temperature, high_temperature, summary_text) VALUES ("{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}")'
+        cursor.execute(insert_to_sql)
+    connection.commit()
+
+
+# this creates a separate table with only linear information (no recurring dates)
+def create_linear():
+    linear_sql = 'CREATE TABLE IF NOT EXISTS weather_linear (date, low_temperature, high_temperature, summary_text)'
+    cursor.execute(linear_sql)
+    connection.commit()
+
+
+# only the 6th latest date is including, where it can be used as reference when comparing actual observed vs the 6th day forecast
+def insert_last_value():
+    df_tail = df.tail(1)
+    for i, row in df_tail.iterrows():
+        insert_to_sql = f'INSERT INTO weather_linear (date, low_temperature, high_temperature, summary_text) VALUES ("{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}")'
+        cursor.execute(insert_to_sql)
+    connection.commit()
+
+
+# execute recurrent
+def weather_legend():
+    create_legend()
+    insert_values()
+    print(pd.read_sql('SELECT * FROM weather_legend', connection))
+
+
+# execute linear
+def weather_linear():
+    create_linear()
+    insert_last_value()
+    print(pd.read_sql('SELECT * FROM weather_linear', connection))
+
+
+weather_legend()
+weather_linear()
+
+connection.close()
