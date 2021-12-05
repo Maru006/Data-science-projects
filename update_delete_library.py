54164e99d8e78815f9e46f96c406eb85497fd2a0
Maru006 <87828918+Maru006@users.noreply.github.com>
2021-12-05 20:05:02 +0000

    Update update_delete_library.py
    Insert, update and Delete all now function correctly (as much as I was aware of correct and incorrect ways people may use them) with debuging outputs I will further update to specify every parameter that may be potentially incorrect.
    
    COMING SOON.....
    I will shortly add a reset ROWID function due to how sqlite3 naturally keeps their ROWID even after deletion: i.e., when a row is deleted, its ROWID practically becomes inaccessible. A separate function was necessary considering iterators might fail if ROWID reindex is set to true for each loop (problematic as I realise).

--- update_delete_library.py
+++ update_delete_library.py
@@ -1,6 +1,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 import pandas as pd
+import pandas.io.sql
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
@@ -9,69 +10,78 @@ import sqlite3
 
 
 def insert(url, table, date, low_temperature, high_temperature, summary_text):
-    connection = sqlite3.connect(url)
-    logging.info('SUCCESSFUL :: Connected to Database')
-    cursor = connection.cursor()
     try:
-        cursor.execute(f'INSERT INTO {table} (date, low_temperature, high_temperature, summary_text) '
-                       f'VALUES ("{date}", "{low_temperature}", "{high_temperature}", "{summary_text}")')
-    except sqlite3.OperationalError:
-        logging.warning(f' {table} does not exist')
-        cursor.execute("SELECT name "
-                       "FROM sqlite_master "
-                       "WHERE type='table'")
-        logging.info(f' Your existing tables are: {cursor.fetchall()}')
-        return None
-    connection.commit()
-    print(pd.read_sql(f'SELECT ROWID, * FROM {table}', connection))
-    connection.close()
-    logging.info(' SUCCESSFUL :: Disconnected to Database')
-    
-    
-def update(url, table, ID, setVar, setVal):
-    connection = sqlite3.connect(url)
-    cursor = connection.cursor()
-    erroneousID = []
-    if ID:
+        connection = sqlite3.connect(url)
+        cursor = connection.cursor()
         try:
-            iter(ID)
-            for i in ID:
-                if isinstance(i, int):
-                    logging.info(f' {i} is OK')
-                    try:
-                        cursor.execute(f'UPDATE {table} '
-                                       f'SET {setVar} = {setVal} WHERE ROWID = {ID}')
-                    except ValueError or TypeError:
-                        break
-                else:
-                    logging.warning(f' {i}  is not an int.')
-                    erroneousID.append(i)
-                    break
+            cursor.execute(f'INSERT INTO {table} (date, low_temperature, high_temperature, summary_text) '
+                           f'VALUES ("{date}", "{low_temperature}", "{high_temperature}", "{summary_text}")')
+        except sqlite3.OperationalError:
+            logging.warning(f' database: {url} or table: {table} may not exist. Check info below:')
+            cursor.execute("SELECT name "
+                           "FROM sqlite_master "
+                           "WHERE type='table'")
+            logging.info(f' In database, "{url}", your existing tables are: {cursor.fetchall()}. If not shown, '
+                         f'your url may not exist or is incorrect')
+            table_info = pd.read_sql(f'SELECT ROWID, * FROM {table}', connection)
+            logging.info(f' In "{table}", your existing columns are: {table_info.columns}. If not shown, '
+                         f'your table may not exist or is incorrect')
+        print(pd.read_sql(f'SELECT ROWID, * FROM {table}', connection))
+        connection.close()
+        logging.info(' SUCCESSFUL :: Disconnected to Database')
+    except pandas.io.sql.DatabaseError:
+        logging.warning(f' Could not connect to {url}. If INFO: your existing tables are not shown, this means your url may not exist or is incorrect')
+
+    
+    
+def update(url, table, setVar, setVal, setID):
+    try:
+        connection = sqlite3.connect(url)
+        cursor = connection.cursor()
+        try:
+            cursor.execute(f'UPDATE {table} SET {setVar} = {setVal} WHERE ROWID = {setID}')
+            connection.commit()
+            logging.info(' SUCCESSFUL :: Disconnected to Database')
+        except sqlite3.Error:
+            logging.warning(f' database: {url}, table: {table} or column: {setVar} may not exist. Check info below:')
+            cursor.execute("SELECT name "
+                           "FROM sqlite_master "
+                           "WHERE type='table'")
+            logging.info(f' In database, "{url}", your existing tables are: {cursor.fetchall()}. If not shown, '
+                         f'your url may not exist or is incorrect')
+            table_info = pd.read_sql(f'SELECT ROWID, * FROM {table}', connection)
+            logging.info(f' In "{table}", your existing columns are: {table_info.columns}. If not shown, '
+                         f'your table may not exist or is incorrect')
+        logging.info('If the table does not update, note that the ROWID (which may need to be reset) may not exist')
+        print(pd.read_sql(f'SELECT ROWID, * FROM {table}', connection))
+        connection.close()
+        logging.info(' SUCCESSFUL :: Disconnected to Database')
+    except pandas.io.sql.DatabaseError:
+        logging.warning(f' Could not connect to {url}. If INFO: your existing tables are not shown, this means your url may not exist or is incorrect')
+
+
+
+def delete(url, table, setID):
+    try:
+        connection = sqlite3.connect(url)
+        cursor = connection.cursor()
+        try:
+            if isinstance(setID, int):
+                cursor.execute(f'DELETE FROM {table} WHERE ROWID = {setID}')
             else:
-                if len(erroneousID) == 1:
-                    logging.warning(' To update cells, specify ROWID only with an int')
-                else:
-                    logging.warning(f'{erroneousID} are not integers.'
-                                    f' To update cells, specify ROWID only with an int')
-        except TypeError:
-            logging.warning(f' {ID} must be an int or iterable.'
-                            f'To update cells, specify ROWID')
-    connection.commit()
-    print(pd.read_sql(f'SELECT * FROM {table}', connection))
-    connection.close()
+                logging.warning(f' {setID} is a {type(setID)}. Value must be a unit of int. '
+                                f'Try enclosing your argument inside an iterable function instead.')
+        except sqlite3.OperationalError:
+            logging.warning(f' {table} may not exist')
+            cursor.execute("SELECT name "
+                           "FROM sqlite_master "
+                           "WHERE type='table'")
+            logging.info(f' Your existing tables are: {cursor.fetchall()}')
+        connection.commit()
+        logging.info('If the table does not update, note that the ROWID may not exist.')
+        print(pd.read_sql(f'SELECT ROWID, * FROM {table}', connection))
+        connection.close()
+        logging.info(' SUCCESSFUL :: Disconnected to Database')
+    except pandas.io.sql.DatabaseError:
+        logging.warning(f' Could not connect to {url}. If INFO: your existing tables are not shown, this means your url may not exist or is incorrect')
 
-
-def delete_cells(url, table, setVal):
-    connection = sqlite3.connect(url)
-    logging.info(' SUCCESSFUL :: Connected to Database')
-    cursor = connection.cursor()
-    if isinstance(setVal, int):
-        cursor.execute(f'DELETE FROM {table} WHERE ROWID = {setVal}')
-    else:
-        logging.warning(f' {setVal} is a {type(setVal)}. Value must be a unit of int. '
-                        f'Try enclosing your argument inside an iterable function instead.')
-    connection.commit()
-    logging.info('If the table does not update, note that the ROWID may not exist after deletion')
-    print(pd.read_sql(f'SELECT ROWID, * FROM {table}', connection))
-    connection.close()
-    logging.info(' SUCCESSFUL :: Disconnected to Database')
