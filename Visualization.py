a7dc2364c907aca38b9fd33851b6e9dd047a598c
Maru006 <87828918+Maru006@users.noreply.github.com>
2021-12-08 20:55:46 +0000

    Update Visualization.py
    You can now customize comparison! between any number of days with data from any of the 1 to 7 forecast days in advance in advance!!! just type:
    
    comparison([1,5,3,7])
    ...and a scatter plot of data taken nth days in advance will be shown for highest and lowest predicted temperatures!

--- Visualization.py
+++ Visualization.py
@@ -70,21 +70,73 @@ def legendPlot():
     plt.title('Mean-Legend temperature readings')
     plt.show()
 
+def comparison(compare, focus=False):
+    included_dates = []
+    try:
+        iter(compare)
+        try:
+            maxim = max(compare)
+            minim = min(compare)
+            if maxim <= 7 and minim >= 1:
+                for x in iter(compare):
+                    dates = data_legend.loc[np.where(data_legend['frequency'] == x)].date
+                    for date in dates:
+                        included_dates.append(data_legend.loc[data_legend['date'] == date])
+                    print(f'There were a total of {len(dates)} dates with frequency: {x}')
+            else:
+                print(f'The maximum length for a forecast is 7, and the minimum is 1.'
+                      f'\n You have given a max of: {maxim} and a minimum of :{minim}')
+                return None
+        except TypeError:
+            print('One of your values is not an integer')
+            return None
+    except TypeError:
+        print(f' {type(compare)} is not iterable. Comparisons can be made with an iterable specifier')
+        return None
+    included_dates = pd.concat(included_dates)
 
-# partitions dates which have 7 total observations from dates such as today and 6 days in advance that have only 1 variation
-dates = list(data_legend.loc[np.where(data_legend['frequency'] == 5)].date)
-sevens = []
-for i in dates:
-    values = data_legend.loc[data_legend['date'] == i]
-    sevens.append(values)
-sevens = pd.concat(sevens)
-sevens_1 = sevens.loc[sevens['frequency'] == 1]
-sevens_7 = sevens.loc[sevens['frequency'] == 5]
+    # visualization
+    if focus is False:
+        fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
+        for comp in iter(compare):
+            ax1.scatter(included_dates.iloc[np.where(included_dates['frequency'] == comp)].date,
+                        included_dates.iloc[np.where(included_dates['frequency'] == comp)].high_temperature)
+            ax1.set_xticklabels(labels=[])
+            ax2.scatter(included_dates.iloc[np.where(included_dates['frequency'] == comp)].date,
+                        included_dates.iloc[np.where(included_dates['frequency'] == comp)].low_temperature,
+                        label=f'{comp} days in advanced')
 
+        fig.supylabel('Temperature')
+        fig.supxlabel('Dates')
+        plt.get_figlabels()  # still confusing to implement
+        plt.legend(loc='best')
+        plt.subplots_adjust(hspace=0.05)
+        plt.suptitle(f'Custom-comparison between predicted temperatures')
+        plt.show()
+    if focus is True:
+        pass
+        # to be implemented in the future.
+        # ideally this would be only 1 figure with every element in it. This makes it possible to see how far or close temperatures...
+        # become as the predicted days grow. We may even see a point where the highest temperature reading at frequency 1 is as low as the...
+        # lowest temperature reading at frequency 7.
+        # the major problem is how I could use labels to tell them apart from each other....
+        # I am also thinking about using set combinations and have them all at seperate figures... thats would be fun!
 
-# run these section for weather-variability with complete forecast n=7, N=77
+        
 # a boxplot difference between 6-day forecast temperatures vs actual temperature (low and high)
 def box_predicted_7vs1_actual():
+    
+    # partitions dates which have 7 total observations from dates such as today and 6 days in advance that have only 1 variation
+    dates = list(data_legend.loc[np.where(data_legend['frequency'] == 5)].date)
+    sevens = []
+    for i in dates:
+        values = data_legend.loc[data_legend['date'] == i]
+        sevens.append(values)
+    sevens = pd.concat(sevens)
+    sevens_1 = sevens.loc[sevens['frequency'] == 1]
+    sevens_7 = sevens.loc[sevens['frequency'] == 5]
+    
+    #visualization
     fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
     fig.supylabel('Temperature')
     fig.suptitle('Difference between Forecast and Actual recorded temperature')
@@ -105,35 +157,10 @@ def box_predicted_7vs1_actual():
     plt.show()
 
 
-# a similar comparison over-time.
-def line_predicted_7vs1_actual():
-    fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
-    fig.supylabel('Temperature')
-    fig.supxlabel('Dates')
-    fig.suptitle('Contrast between Forecast and Actual recorded temperature')
-
-    print(sevens_1)
-    ax1.plot(sevens_1['date'], sevens_1['low_temperature'], '.-', label='Actual')
-    ax1.plot(sevens_7['date'], sevens_7['low_temperature'], '.-', label='Predicted')
-    ax1.set_title('Lowest Temperature')
-    ax1.set_xticklabels(labels=[])
-    ax1.legend()
-
-    ax2.plot(sevens_1['date'], sevens_1['high_temperature'], '.-')
-    ax2.plot(sevens_7['date'], sevens_7['high_temperature'], '.-')
-    ax2.set_title('Highest Temperature')
-
-    plt.annotate('predicted temperatures are 6 days in advance',
-                 xy=(sevens_1['date'][27], 10),
-                 fontsize=10)
-    plt.subplots_adjust(hspace=.05)
-    plt.tight_layout()
-    plt.show()
-
 
 if __name__ == '__main__':
     legendPlot()
     box_predicted_7vs1_actual()
-    line_predicted_7vs1_actual()
+    comparison([1,7]) # by default this is the comparison. But you can add more than two numbers.
     connection.close()
 
